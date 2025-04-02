3aaabe226245cae01db11d7c7a32a8c6b8da12c7
Juniel Hssn <junielhussain@gmail.com>
2025-04-02 20:50:54 +0800

    spartan ui installed

--- tasktracker-fe/src/app/app.component.ts
+++ tasktracker-fe/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmButtonDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'tasktracker-fe';
